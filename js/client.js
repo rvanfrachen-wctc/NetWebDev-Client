bcddadb5d75545a9d3b2af8b119576489a413966
Ryan Van Frachen <rvanfrachen@my.wctc.wdu>
2022-01-27 10:44:36 -0600

    Module 2

--- js/client.js
+++ js/client.js
@@ -10,6 +10,16 @@ $(function () {
         toast.currentTime = 0;
         // play audio
         toast.play();
+        // display product data
+        $('#product').html($(this).data('product'));
+        $('#code').html($(this).data('code'));
         $('#toast').toast({ autohide: false }).toast('show');
+    });
+
+    // Close toast on esc key
+    $(document).on('keyup', function(event) {
+        if (event.key == "Escape") {
+            $('#toast').toast('hide');
+        }
     });
 });
