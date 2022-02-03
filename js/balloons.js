3bc12db12b830d783102ca81925ceac12ebd676d
Ryan Van Frachen <rvanfrachen@my.wctc.edu>
2022-02-03 08:42:59 -0600

    Module 3 - Fixed Balloon Animations and Added

--- js/balloons.js
+++ js/balloons.js
@@ -12,7 +12,10 @@ $(function(){
         $('#' + this.id + 'Img').css('visibility', 'visible')
         // animate balloon in/out based on checkbox
         $(this).is(':checked') ?
-         $('#' + this.id + 'Img').removeClass().addClass('animated bounceInDown') :
-         $('#' + this.id + 'Img').addClass('animated bounceOutUp');
+         $('#' + this.id + 'Img').removeClass().removeClass().addClass('animate__animated').addClass('animate__bounceInDown') :
+         $('#' + this.id + 'Img').addClass('animate__animated').addClass('animate__bounceOutUp');
     });
+
+    const element = document.querySelector('h1#happyBirthday');
+    element.classList.add('animate__animated', 'animate__bounceOutLeft');
 });
